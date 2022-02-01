20d6eae9bdde99cb055333ddb7b7e4f5a0175707
MeetManvar <meet.manvar2010@gmail.com>
2022-02-01 12:17:41 +0530

    updated calculate.sh

--- calculate.sh
+++ calculate.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-# take two integers from the user
 echo "Enter two integers: "
 read a b
 
-# perform Substraction
 result=`expr $a + $b`
-
-# show result
 echo "Result of addition: $result"
 
-# compute division result
 result=`expr "$a * $b" | bc -l`
-
-# print output
 echo "Result of multiplication: $result"
